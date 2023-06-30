4e9f32f624f3797f6524274c68ce1c48d99a4372
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-06-29 22:18:42 -0300

    Parte de logica em JS Concluida

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Estou funcionando')
+const openSidebar = document.querySelector('.open-sidebar')
+const closeSidebar = document.querySelector('.close-sidebar')
+const sidebar = document.querySelector('.blackout')
+
+openSidebar.addEventListener('click', () => {
+  sidebar.classList.add('show')
+})
+
+closeSidebar.addEventListener('click', () => {
+  sidebar.classList.remove('show')
+})
